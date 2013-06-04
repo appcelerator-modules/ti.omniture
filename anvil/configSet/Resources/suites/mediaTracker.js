0e6e618bfc0846c7dc3c8d94756e99dc58a5978e
jonalter <jonalter+github@gmail.com>
2013-06-03 17:56:23 -0700

    [MOD-1373] Omniture: iOS: fixing spacing on anvil tests

--- anvil/configSet/Resources/suites/mediaTracker.js
+++ anvil/configSet/Resources/suites/mediaTracker.js
@@ -7,216 +7,216 @@
 
 module.exports = new function ()
 {
-	var finish;
-	var valueOf;
-	var Omniture;
-	
-	this.init = function (testUtils)
-	{
-		finish = testUtils.finish;
-		valueOf = testUtils.valueOf;
-		Omniture = require('ti.omniture');
-	};
+    var finish;
+    var valueOf;
+    var Omniture;
+    
+    this.init = function (testUtils)
+    {
+        finish = testUtils.finish;
+        valueOf = testUtils.valueOf;
+        Omniture = require('ti.omniture');
+    };
 
-	this.name = "mediaTracker";
-	
-	// ---------------------------------------------------------------
-	// mediaTracker
-	// ---------------------------------------------------------------
+    this.name = "mediaTracker";
+    
+    // ---------------------------------------------------------------
+    // mediaTracker
+    // ---------------------------------------------------------------
 
-	this.testFunctions = function (testRun)
-	{
-		valueOf(testRun, function() {
-			Omniture.startMediaTracker();
-		}).shouldNotThrowException();
+    this.testFunctions = function (testRun)
+    {
+        valueOf(testRun, function() {
+            Omniture.startMediaTracker();
+        }).shouldNotThrowException();
 
-		finish(testRun);
-	}
+        finish(testRun);
+    }
 
-	this.testProperties = function (testRun)
-	{
-		var mediaTracker = Omniture.startMediaTracker();
+    this.testProperties = function (testRun)
+    {
+        var mediaTracker = Omniture.startMediaTracker();
 
-		valueOf(testRun, mediaTracker.trackVars).shouldBe("None");
-		valueOf(testRun, mediaTracker.trackEvents).shouldBe("None");
-		valueOf(testRun, mediaTracker.channel).shouldBeUndefined();
-		valueOf(testRun, mediaTracker.completeCloseOffsetThreshold).shouldBeNumber();
-		valueOf(testRun, mediaTracker.contextDataMapping).shouldBeUndefined();
+        valueOf(testRun, mediaTracker.trackVars).shouldBe("None");
+        valueOf(testRun, mediaTracker.trackEvents).shouldBe("None");
+        valueOf(testRun, mediaTracker.channel).shouldBeUndefined();
+        valueOf(testRun, mediaTracker.completeCloseOffsetThreshold).shouldBeNumber();
+        valueOf(testRun, mediaTracker.contextDataMapping).shouldBeUndefined();
 
-		valueOf(testRun, mediaTracker.trackSeconds).shouldBeNumber();
-		valueOf(testRun, mediaTracker.trackMilestones).shouldBeUndefined();
-		valueOf(testRun, mediaTracker.segmentByMilestones).shouldBeBoolean();
-		valueOf(testRun, mediaTracker.trackOffsetMilestones).shouldBeUndefined();
-		valueOf(testRun, mediaTracker.segmentByOffsetMilestones).shouldBeBoolean();
+        valueOf(testRun, mediaTracker.trackSeconds).shouldBeNumber();
+        valueOf(testRun, mediaTracker.trackMilestones).shouldBeUndefined();
+        valueOf(testRun, mediaTracker.segmentByMilestones).shouldBeBoolean();
+        valueOf(testRun, mediaTracker.trackOffsetMilestones).shouldBeUndefined();
+        valueOf(testRun, mediaTracker.segmentByOffsetMilestones).shouldBeBoolean();
 
-		valueOf(testRun, mediaTracker.adTrackSeconds).shouldBeNumber();
-		valueOf(testRun, mediaTracker.adTrackMilestones).shouldBeUndefined();
-		valueOf(testRun, mediaTracker.adSegmentByMilestones).shouldBeBoolean();
-		valueOf(testRun, mediaTracker.adTrackOffsetMilestones).shouldBeUndefined();
-		valueOf(testRun, mediaTracker.adSegmentByOffsetMilestones).shouldBeBoolean();
+        valueOf(testRun, mediaTracker.adTrackSeconds).shouldBeNumber();
+        valueOf(testRun, mediaTracker.adTrackMilestones).shouldBeUndefined();
+        valueOf(testRun, mediaTracker.adSegmentByMilestones).shouldBeBoolean();
+        valueOf(testRun, mediaTracker.adTrackOffsetMilestones).shouldBeUndefined();
+        valueOf(testRun, mediaTracker.adSegmentByOffsetMilestones).shouldBeBoolean();
 
-		finish(testRun);
-	};
+        finish(testRun);
+    };
 
-	// Test that all of the Properties are defined
-	this.testPropertiesSetWithStartMediaTracker = function (testRun)
-	{
-		var trackVars = "trackVars",
-			trackEvents = "trackEvents",
-			channel = "channel",
-			completeCloseOffsetThreshold = 2,
-			contextDataMapping = {hello: "there"},
+    // Test that all of the Properties are defined
+    this.testPropertiesSetWithStartMediaTracker = function (testRun)
+    {
+        var trackVars = "trackVars",
+            trackEvents = "trackEvents",
+            channel = "channel",
+            completeCloseOffsetThreshold = 2,
+            contextDataMapping = {hello: "there"},
 
-			trackSeconds = 21,
-			trackMilestones = "trackMilestones",
-			segmentByMilestones = true,
-			trackOffsetMilestones = "trackOffsetMilestones",
-			segmentByOffsetMilestones = true,
+            trackSeconds = 21,
+            trackMilestones = "trackMilestones",
+            segmentByMilestones = true,
+            trackOffsetMilestones = "trackOffsetMilestones",
+            segmentByOffsetMilestones = true,
 
-			adTrackSeconds = 19,
-			adTrackMilestones = "adTrackMilestones",
-			adSegmentByMilestones = true,
-			adTrackOffsetMilestones = "adTrackOffsetMilestones",
-			adSegmentByOffsetMilestones = true;
+            adTrackSeconds = 19,
+            adTrackMilestones = "adTrackMilestones",
+            adSegmentByMilestones = true,
+            adTrackOffsetMilestones = "adTrackOffsetMilestones",
+            adSegmentByOffsetMilestones = true;
 
-		var mediaTracker = Omniture.startMediaTracker({
-		    trackVars: trackVars,
-		    trackEvents: trackEvents,
-		    channel: channel,
-		    completeCloseOffsetThreshold: completeCloseOffsetThreshold,
-		    contextDataMapping: contextDataMapping,
+        var mediaTracker = Omniture.startMediaTracker({
+            trackVars: trackVars,
+            trackEvents: trackEvents,
+            channel: channel,
+            completeCloseOffsetThreshold: completeCloseOffsetThreshold,
+            contextDataMapping: contextDataMapping,
 
-		    trackSeconds: trackSeconds,
-		    trackMilestones: trackMilestones,
-		    segmentByMilestones: segmentByMilestones,
-		    trackOffsetMilestones: trackOffsetMilestones,
-		    segmentByOffsetMilestones: segmentByOffsetMilestones,
+            trackSeconds: trackSeconds,
+            trackMilestones: trackMilestones,
+            segmentByMilestones: segmentByMilestones,
+            trackOffsetMilestones: trackOffsetMilestones,
+            segmentByOffsetMilestones: segmentByOffsetMilestones,
 
-		    adTrackSeconds: adTrackSeconds,
-		    adTrackMilestones: adTrackMilestones,
-		    adSegmentByMilestones: adSegmentByMilestones,
-		    adTrackOffsetMilestones: adTrackOffsetMilestones,
-		    adSegmentByOffsetMilestones: adSegmentByOffsetMilestones
-		});
+            adTrackSeconds: adTrackSeconds,
+            adTrackMilestones: adTrackMilestones,
+            adSegmentByMilestones: adSegmentByMilestones,
+            adTrackOffsetMilestones: adTrackOffsetMilestones,
+            adSegmentByOffsetMilestones: adSegmentByOffsetMilestones
+        });
 
-		valueOf(testRun, mediaTracker.trackVars).shouldBe(trackVars);
-		valueOf(testRun, mediaTracker.trackEvents).shouldBe(trackEvents);
-		valueOf(testRun, mediaTracker.channel).shouldBe(channel);
-		valueOf(testRun, mediaTracker.completeCloseOffsetThreshold).shouldBe(completeCloseOffsetThreshold);
-		valueOf(testRun, mediaTracker.contextDataMapping.hello).shouldBe(contextDataMapping.hello);
+        valueOf(testRun, mediaTracker.trackVars).shouldBe(trackVars);
+        valueOf(testRun, mediaTracker.trackEvents).shouldBe(trackEvents);
+        valueOf(testRun, mediaTracker.channel).shouldBe(channel);
+        valueOf(testRun, mediaTracker.completeCloseOffsetThreshold).shouldBe(completeCloseOffsetThreshold);
+        valueOf(testRun, mediaTracker.contextDataMapping.hello).shouldBe(contextDataMapping.hello);
 
-		valueOf(testRun, mediaTracker.trackSeconds).shouldBe(trackSeconds);
-		valueOf(testRun, mediaTracker.trackMilestones).shouldBe(trackMilestones);
-		valueOf(testRun, mediaTracker.segmentByMilestones).shouldBe(segmentByMilestones);
-		valueOf(testRun, mediaTracker.trackOffsetMilestones).shouldBe(trackOffsetMilestones);
-		valueOf(testRun, mediaTracker.segmentByOffsetMilestones).shouldBe(segmentByOffsetMilestones);
+        valueOf(testRun, mediaTracker.trackSeconds).shouldBe(trackSeconds);
+        valueOf(testRun, mediaTracker.trackMilestones).shouldBe(trackMilestones);
+        valueOf(testRun, mediaTracker.segmentByMilestones).shouldBe(segmentByMilestones);
+        valueOf(testRun, mediaTracker.trackOffsetMilestones).shouldBe(trackOffsetMilestones);
+        valueOf(testRun, mediaTracker.segmentByOffsetMilestones).shouldBe(segmentByOffsetMilestones);
 
-		valueOf(testRun, mediaTracker.adTrackSeconds).shouldBe(adTrackSeconds);
-		valueOf(testRun, mediaTracker.adTrackMilestones).shouldBe(adTrackMilestones);
-		valueOf(testRun, mediaTracker.adSegmentByMilestones).shouldBe(adSegmentByMilestones);
-		valueOf(testRun, mediaTracker.adTrackOffsetMilestones).shouldBe(adTrackOffsetMilestones);
-		valueOf(testRun, mediaTracker.adSegmentByOffsetMilestones).shouldBe(adSegmentByOffsetMilestones);
+        valueOf(testRun, mediaTracker.adTrackSeconds).shouldBe(adTrackSeconds);
+        valueOf(testRun, mediaTracker.adTrackMilestones).shouldBe(adTrackMilestones);
+        valueOf(testRun, mediaTracker.adSegmentByMilestones).shouldBe(adSegmentByMilestones);
+        valueOf(testRun, mediaTracker.adTrackOffsetMilestones).shouldBe(adTrackOffsetMilestones);
+        valueOf(testRun, mediaTracker.adSegmentByOffsetMilestones).shouldBe(adSegmentByOffsetMilestones);
 
-		finish(testRun);
-	};
+        finish(testRun);
+    };
 
-	this.testPropertiesSetWithStartMediaTrackerAndOverriddenWithProperties = function (testRun)
-	{
-		var _trackVars = "_trackVars",
-			_trackEvents = "_trackEvents",
-			_channel = "_channel",
-			_completeCloseOffsetThreshold = 12,
-			_contextDataMapping = {hello: "_there"},
+    this.testPropertiesSetWithStartMediaTrackerAndOverriddenWithProperties = function (testRun)
+    {
+        var _trackVars = "_trackVars",
+            _trackEvents = "_trackEvents",
+            _channel = "_channel",
+            _completeCloseOffsetThreshold = 12,
+            _contextDataMapping = {hello: "_there"},
 
-			_trackSeconds = 121,
-			_trackMilestones = "_trackMilestones",
-			_segmentByMilestones = false,
-			_trackOffsetMilestones = "_trackOffsetMilestones",
-			_segmentByOffsetMilestones = false,
+            _trackSeconds = 121,
+            _trackMilestones = "_trackMilestones",
+            _segmentByMilestones = false,
+            _trackOffsetMilestones = "_trackOffsetMilestones",
+            _segmentByOffsetMilestones = false,
 
-			_adTrackSeconds = 119,
-			_adTrackMilestones = "_adTrackMilestones",
-			_adSegmentByMilestones = false,
-			_adTrackOffsetMilestones = "_adTrackOffsetMilestones",
-			_adSegmentByOffsetMilestones = false;
+            _adTrackSeconds = 119,
+            _adTrackMilestones = "_adTrackMilestones",
+            _adSegmentByMilestones = false,
+            _adTrackOffsetMilestones = "_adTrackOffsetMilestones",
+            _adSegmentByOffsetMilestones = false;
 
-		var trackVars = "trackVars",
-			trackEvents = "trackEvents",
-			channel = "channel",
-			completeCloseOffsetThreshold = 2,
-			contextDataMapping = {hello: "there"},
+        var trackVars = "trackVars",
+            trackEvents = "trackEvents",
+            channel = "channel",
+            completeCloseOffsetThreshold = 2,
+            contextDataMapping = {hello: "there"},
 
-			trackSeconds = 21,
-			trackMilestones = "trackMilestones",
-			segmentByMilestones = true,
-			trackOffsetMilestones = "trackOffsetMilestones",
-			segmentByOffsetMilestones = true,
+            trackSeconds = 21,
+            trackMilestones = "trackMilestones",
+            segmentByMilestones = true,
+            trackOffsetMilestones = "trackOffsetMilestones",
+            segmentByOffsetMilestones = true,
 
-			adTrackSeconds = 19,
-			adTrackMilestones = "adTrackMilestones",
-			adSegmentByMilestones = true,
-			adTrackOffsetMilestones = "adTrackOffsetMilestones",
-			adSegmentByOffsetMilestones = true;
+            adTrackSeconds = 19,
+            adTrackMilestones = "adTrackMilestones",
+            adSegmentByMilestones = true,
+            adTrackOffsetMilestones = "adTrackOffsetMilestones",
+            adSegmentByOffsetMilestones = true;
 
-		var mediaTracker = Omniture.startMediaTracker({
-		    trackVars: _trackVars,
-		    trackEvents: _trackEvents,
-		    channel: _channel,
-		    completeCloseOffsetThreshold: _completeCloseOffsetThreshold,
-		    contextDataMapping: _contextDataMapping,
+        var mediaTracker = Omniture.startMediaTracker({
+            trackVars: _trackVars,
+            trackEvents: _trackEvents,
+            channel: _channel,
+            completeCloseOffsetThreshold: _completeCloseOffsetThreshold,
+            contextDataMapping: _contextDataMapping,
 
-		    trackSeconds: _trackSeconds,
-		    trackMilestones: _trackMilestones,
-		    segmentByMilestones: _segmentByMilestones,
-		    trackOffsetMilestones: _trackOffsetMilestones,
-		    segmentByOffsetMilestones: _segmentByOffsetMilestones,
+            trackSeconds: _trackSeconds,
+            trackMilestones: _trackMilestones,
+            segmentByMilestones: _segmentByMilestones,
+            trackOffsetMilestones: _trackOffsetMilestones,
+            segmentByOffsetMilestones: _segmentByOffsetMilestones,
 
-		    adTrackSeconds: _adTrackSeconds,
-		    adTrackMilestones: _adTrackMilestones,
-		    adSegmentByMilestones: _adSegmentByMilestones,
-		    adTrackOffsetMilestones: _adTrackOffsetMilestones,
-		    adSegmentByOffsetMilestones: _adSegmentByOffsetMilestones
-		});
+            adTrackSeconds: _adTrackSeconds,
+            adTrackMilestones: _adTrackMilestones,
+            adSegmentByMilestones: _adSegmentByMilestones,
+            adTrackOffsetMilestones: _adTrackOffsetMilestones,
+            adSegmentByOffsetMilestones: _adSegmentByOffsetMilestones
+        });
 
-		mediaTracker.trackVars = trackVars;
-		mediaTracker.trackEvents = trackEvents;
-		mediaTracker.channel = channel;
-		mediaTracker.completeCloseOffsetThreshold = completeCloseOffsetThreshold;
-		mediaTracker.contextDataMapping = contextDataMapping;
+        mediaTracker.trackVars = trackVars;
+        mediaTracker.trackEvents = trackEvents;
+        mediaTracker.channel = channel;
+        mediaTracker.completeCloseOffsetThreshold = completeCloseOffsetThreshold;
+        mediaTracker.contextDataMapping = contextDataMapping;
 
-		mediaTracker.trackSeconds = trackSeconds;
-		mediaTracker.trackMilestones = trackMilestones;
-		mediaTracker.segmentByMilestones = segmentByMilestones;
-		mediaTracker.trackOffsetMilestones = trackOffsetMilestones;
-		mediaTracker.segmentByOffsetMilestones = segmentByOffsetMilestones;
+        mediaTracker.trackSeconds = trackSeconds;
+        mediaTracker.trackMilestones = trackMilestones;
+        mediaTracker.segmentByMilestones = segmentByMilestones;
+        mediaTracker.trackOffsetMilestones = trackOffsetMilestones;
+        mediaTracker.segmentByOffsetMilestones = segmentByOffsetMilestones;
 
-		mediaTracker.adTrackSeconds = adTrackSeconds;
-		mediaTracker.adTrackMilestones = adTrackMilestones;
-		mediaTracker.adSegmentByMilestones = adSegmentByMilestones;
-		mediaTracker.adTrackOffsetMilestones = adTrackOffsetMilestones;
-		mediaTracker.adSegmentByOffsetMilestones = adSegmentByOffsetMilestones;
+        mediaTracker.adTrackSeconds = adTrackSeconds;
+        mediaTracker.adTrackMilestones = adTrackMilestones;
+        mediaTracker.adSegmentByMilestones = adSegmentByMilestones;
+        mediaTracker.adTrackOffsetMilestones = adTrackOffsetMilestones;
+        mediaTracker.adSegmentByOffsetMilestones = adSegmentByOffsetMilestones;
 
-		valueOf(testRun, mediaTracker.trackVars).shouldBe(trackVars);
-		valueOf(testRun, mediaTracker.trackEvents).shouldBe(trackEvents);
-		valueOf(testRun, mediaTracker.channel).shouldBe(channel);
-		valueOf(testRun, mediaTracker.completeCloseOffsetThreshold).shouldBe(completeCloseOffsetThreshold);
-		valueOf(testRun, mediaTracker.contextDataMapping.hello).shouldBe(contextDataMapping.hello);
+        valueOf(testRun, mediaTracker.trackVars).shouldBe(trackVars);
+        valueOf(testRun, mediaTracker.trackEvents).shouldBe(trackEvents);
+        valueOf(testRun, mediaTracker.channel).shouldBe(channel);
+        valueOf(testRun, mediaTracker.completeCloseOffsetThreshold).shouldBe(completeCloseOffsetThreshold);
+        valueOf(testRun, mediaTracker.contextDataMapping.hello).shouldBe(contextDataMapping.hello);
 
-		valueOf(testRun, mediaTracker.trackSeconds).shouldBe(trackSeconds);
-		valueOf(testRun, mediaTracker.trackMilestones).shouldBe(trackMilestones);
-		valueOf(testRun, mediaTracker.segmentByMilestones).shouldBe(segmentByMilestones);
-		valueOf(testRun, mediaTracker.trackOffsetMilestones).shouldBe(trackOffsetMilestones);
-		valueOf(testRun, mediaTracker.segmentByOffsetMilestones).shouldBe(segmentByOffsetMilestones);
+        valueOf(testRun, mediaTracker.trackSeconds).shouldBe(trackSeconds);
+        valueOf(testRun, mediaTracker.trackMilestones).shouldBe(trackMilestones);
+        valueOf(testRun, mediaTracker.segmentByMilestones).shouldBe(segmentByMilestones);
+        valueOf(testRun, mediaTracker.trackOffsetMilestones).shouldBe(trackOffsetMilestones);
+        valueOf(testRun, mediaTracker.segmentByOffsetMilestones).shouldBe(segmentByOffsetMilestones);
 
-		valueOf(testRun, mediaTracker.adTrackSeconds).shouldBe(adTrackSeconds);
-		valueOf(testRun, mediaTracker.adTrackMilestones).shouldBe(adTrackMilestones);
-		valueOf(testRun, mediaTracker.adSegmentByMilestones).shouldBe(adSegmentByMilestones);
-		valueOf(testRun, mediaTracker.adTrackOffsetMilestones).shouldBe(adTrackOffsetMilestones);
-		valueOf(testRun, mediaTracker.adSegmentByOffsetMilestones).shouldBe(adSegmentByOffsetMilestones);
+        valueOf(testRun, mediaTracker.adTrackSeconds).shouldBe(adTrackSeconds);
+        valueOf(testRun, mediaTracker.adTrackMilestones).shouldBe(adTrackMilestones);
+        valueOf(testRun, mediaTracker.adSegmentByMilestones).shouldBe(adSegmentByMilestones);
+        valueOf(testRun, mediaTracker.adTrackOffsetMilestones).shouldBe(adTrackOffsetMilestones);
+        valueOf(testRun, mediaTracker.adSegmentByOffsetMilestones).shouldBe(adSegmentByOffsetMilestones);
 
-		finish(testRun);
-	};
+        finish(testRun);
+    };
 
-	// Populate the array of tests based on the 'hammer' convention
-	this.tests = require('hammer').populateTests(this);
+    // Populate the array of tests based on the 'hammer' convention
+    this.tests = require('hammer').populateTests(this);
 };
--- anvil/configSet/Resources/suites/omniture.js
+++ anvil/configSet/Resources/suites/omniture.js
@@ -7,72 +7,72 @@
 
 module.exports = new function ()
 {
-	var finish;
-	var valueOf;
-	var Omniture;
-	
-	this.init = function (testUtils)
-	{
-		finish = testUtils.finish;
-		valueOf = testUtils.valueOf;
-		Omniture = require('ti.omniture');
-	};
+    var finish;
+    var valueOf;
+    var Omniture;
+    
+    this.init = function (testUtils)
+    {
+        finish = testUtils.finish;
+        valueOf = testUtils.valueOf;
+        Omniture = require('ti.omniture');
+    };
 
-	this.name = "omniture";
-	
-	// ---------------------------------------------------------------
-	// omniture
-	// ---------------------------------------------------------------
+    this.name = "omniture";
+    
+    // ---------------------------------------------------------------
+    // omniture
+    // ---------------------------------------------------------------
 
-	// Test that module is loaded
-	this.testModule = function (testRun)
-	{
-		// Verify that the module is defined
-		valueOf(testRun, Omniture).shouldBeObject();
-		finish(testRun);
-	};
-	
-	// Test that all of the Functions are defined
-	this.testFunctions = function (testRun)
-	{
-		valueOf(testRun, Omniture.startSession).shouldBeFunction();
-		valueOf(testRun, Omniture.startMediaTracker).shouldBeFunction();
+    // Test that module is loaded
+    this.testModule = function (testRun)
+    {
+        // Verify that the module is defined
+        valueOf(testRun, Omniture).shouldBeObject();
+        finish(testRun);
+    };
+    
+    // Test that all of the Functions are defined
+    this.testFunctions = function (testRun)
+    {
+        valueOf(testRun, Omniture.startSession).shouldBeFunction();
+        valueOf(testRun, Omniture.startMediaTracker).shouldBeFunction();
 
-		finish(testRun);
-	};
+        finish(testRun);
+    };
 
-	// Test that all of the Properties are defined
-	this.testProperties = function (testRun)
-	{
-		valueOf(testRun, Omniture.version).shouldBeString();
-		valueOf(testRun, Omniture.debugLogging).shouldBeBoolean();
+    // Test that all of the Properties are defined
+    this.testProperties = function (testRun)
+    {
+        valueOf(testRun, Omniture.version).shouldBeString();
+        valueOf(testRun, Omniture.debugLogging).shouldBeBoolean();
 
-		finish(testRun);
-	};
+        finish(testRun);
+    };
 
-	// calling session or mediaTracker multiple times with dif properties should be set
-	this.testStartSessionCalledMultipleTimes = function (testRun)
-	{
-		var visitorID = "visitorID",
-			eVar1 = "E ONE";
+    // calling session or mediaTracker multiple times with dif properties should be set
+    this.testStartSessionCalledMultipleTimes = function (testRun)
+    {
+        var visitorID = "visitorID",
+            eVar1 = "E ONE";
 
-		var session = Omniture.startSession();
+        var session = Omniture.startSession();
 
-		valueOf(testRun, session.visitorID).shouldBeString();
-		valueOf(testRun, session.visitorID).shouldNotBe(visitorID);
-		valueOf(testRun, session.eVar1).shouldBeUndefined();
+        valueOf(testRun, session.visitorID).shouldBeString();
+        valueOf(testRun, session.visitorID).shouldNotBe(visitorID);
+        valueOf(testRun, session.eVar1).shouldBeUndefined();
 
-		var session2 = Omniture.startSession({
-			visitorID: visitorID,
-			eVar1: eVar1
-		});
+        var session2 = Omniture.startSession({
+            visitorID: visitorID,
+            eVar1: eVar1
+        });
 
-		valueOf(testRun, session.visitorID).shouldBe(visitorID);
-		valueOf(testRun, session.eVar1).shouldBe(eVar1);
-		
-		finish(testRun);
-	};
+        valueOf(testRun, session.visitorID).shouldBe(visitorID);
+        valueOf(testRun, session.eVar1).shouldBe(eVar1);
+        
+        finish(testRun);
+    };
 
-	// Populate the array of tests based on the 'hammer' convention
-	this.tests = require('hammer').populateTests(this);
+    // Populate the array of tests based on the 'hammer' convention
+    this.tests = require('hammer').populateTests(this);
 };
