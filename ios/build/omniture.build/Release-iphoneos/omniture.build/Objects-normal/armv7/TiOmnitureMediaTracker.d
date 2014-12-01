06fb20e6bf27705f99de8038b429641017def95e
Muhammad Dadu <muhammad@dadu.co>
2014-12-01 14:17:55 -0800

    [MOD-1795] Split Omniture into seperate repository

---
+++ ios/build/omniture.build/Release-iphoneos/omniture.build/Objects-normal/armv7/TiOmnitureMediaTracker.d
@@ -0,0 +1,33 @@
+dependencies: \
+  /Users/mdadu/Documents/MuhammadDaduGit/MOD-1759/git/ti.omniture/ios/Classes/TiOmnitureMediaTracker.m \
+  /Users/mdadu/Documents/MuhammadDaduGit/MOD-1759/git/ti.omniture/ios/Classes/TiOmnitureMediaTracker.h \
+  /Users/mdadu/Library/Application\ Support/Titanium/mobilesdk/osx/3.0.0.GA/iphone/include/TiProxy.h \
+  /Users/mdadu/Library/Application\ Support/Titanium/mobilesdk/osx/3.0.0.GA/iphone/include/TiBase.h \
+  /Users/mdadu/Library/Application\ Support/Titanium/mobilesdk/osx/3.0.0.GA/iphone/include/defines.h \
+  /Users/mdadu/Library/Application\ Support/Titanium/mobilesdk/osx/3.0.0.GA/iphone/include/TiThreading.h \
+  /Users/mdadu/Library/Application\ Support/Titanium/mobilesdk/osx/3.0.0.GA/iphone/include/TiPublicAPI.h \
+  /Users/mdadu/Library/Application\ Support/Titanium/mobilesdk/osx/3.0.0.GA/iphone/include/TiEvaluator.h \
+  /Users/mdadu/Library/Application\ Support/Titanium/mobilesdk/osx/3.0.0.GA/iphone/include/KrollContext.h \
+  /Users/mdadu/Library/Application\ Support/Titanium/mobilesdk/osx/3.0.0.GA/iphone/include/TiCore/TiCore.h \
+  /Users/mdadu/Library/Application\ Support/Titanium/mobilesdk/osx/3.0.0.GA/iphone/include/TiCore/Ti.h \
+  /Users/mdadu/Library/Application\ Support/Titanium/mobilesdk/osx/3.0.0.GA/iphone/include/TiCore/TiBase.h \
+  /Users/mdadu/Library/Application\ Support/Titanium/mobilesdk/osx/3.0.0.GA/iphone/include/TiCore/TiContextRef.h \
+  /Users/mdadu/Library/Application\ Support/Titanium/mobilesdk/osx/3.0.0.GA/iphone/include/TiCore/TiObjectRef.h \
+  /Users/mdadu/Library/Application\ Support/Titanium/mobilesdk/osx/3.0.0.GA/iphone/include/TiCore/TiValueRef.h \
+  /Users/mdadu/Library/Application\ Support/Titanium/mobilesdk/osx/3.0.0.GA/iphone/include/TiCore/WebKitAvailability.h \
+  /Users/mdadu/Library/Application\ Support/Titanium/mobilesdk/osx/3.0.0.GA/iphone/include/TiCore/TiStringRef.h \
+  /Users/mdadu/Library/Application\ Support/Titanium/mobilesdk/osx/3.0.0.GA/iphone/include/TiCore/TiStringRefCF.h \
+  /Users/mdadu/Library/Application\ Support/Titanium/mobilesdk/osx/3.0.0.GA/iphone/include/TiCore/TiContextRefPrivate.h \
+  /Users/mdadu/Library/Application\ Support/Titanium/mobilesdk/osx/3.0.0.GA/iphone/include/KrollCallback.h \
+  /Users/mdadu/Library/Application\ Support/Titanium/mobilesdk/osx/3.0.0.GA/iphone/include/KrollObject.h \
+  /Users/mdadu/Library/Application\ Support/Titanium/mobilesdk/osx/3.0.0.GA/iphone/include/TiBindingRunLoop.h \
+  /Users/mdadu/Library/Application\ Support/Titanium/mobilesdk/osx/3.0.0.GA/iphone/include/TiUtils.h \
+  /Users/mdadu/Library/Application\ Support/Titanium/mobilesdk/osx/3.0.0.GA/iphone/include/TiColor.h \
+  /Users/mdadu/Library/Application\ Support/Titanium/mobilesdk/osx/3.0.0.GA/iphone/include/TiDimension.h \
+  /Users/mdadu/Library/Application\ Support/Titanium/mobilesdk/osx/3.0.0.GA/iphone/include/WebFont.h \
+  /Users/mdadu/Library/Application\ Support/Titanium/mobilesdk/osx/3.0.0.GA/iphone/include/TiFile.h \
+  /Users/mdadu/Library/Application\ Support/Titanium/mobilesdk/osx/3.0.0.GA/iphone/include/TiBuffer.h \
+  /Users/mdadu/Library/Application\ Support/Titanium/mobilesdk/osx/3.0.0.GA/iphone/include/TiBlob.h \
+  /Users/mdadu/Documents/MuhammadDaduGit/MOD-1759/git/ti.omniture/ios/Classes/Utils.h \
+  /Users/mdadu/Documents/MuhammadDaduGit/MOD-1759/git/ti.omniture/ios/ADMS_AppLibrary/ADMS_MediaMeasurement.h \
+  /Users/mdadu/Documents/MuhammadDaduGit/MOD-1759/git/ti.omniture/ios/ADMS_AppLibrary/ADMS_Measurement.h
---
+++ ios/build/omniture.build/Release-iphonesimulator/omniture.build/Objects-normal/i386/TiOmnitureModuleAssets.d
@@ -0,0 +1,3 @@
+dependencies: \
+  /Users/mdadu/Documents/MuhammadDaduGit/MOD-1759/git/ti.omniture/ios/Classes/TiOmnitureModuleAssets.m \
+  /Users/mdadu/Documents/MuhammadDaduGit/MOD-1759/git/ti.omniture/ios/Classes/TiOmnitureModuleAssets.h
